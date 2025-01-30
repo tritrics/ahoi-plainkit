ff1847439bbb1c45e08352c1eeeab688f9c9e2cb
tritrics <ma@tritrics.dk>
2025-01-30 16:37:57 +0100

    add vue folder files

--- vue/vite.config.js
+++ vue/vite.config.js
@@ -1,6 +1,5 @@
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
-import { viteStaticCopy } from 'vite-plugin-static-copy'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
@@ -15,14 +14,6 @@ export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: './hostconfig.js',
-          dest: './',
-        },
-      ],
-    }),
   ],
   server: {
     host: true,
